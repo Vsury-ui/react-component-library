0c3ae4bb5f13efaf80065a9d2fc76250844a9c32
Vivek Satish Suryawanshi <viveksatishsuryawanshi@vss.local>
2025-07-02 11:06:49 +0530

    first commit

---
+++ src/components/button/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Button({title, variant = 'contained'}: ButtonProps) {
+
+    let styles: any = {
+        padding: "5px 10px"
+    }
+
+    if(variant === 'outlined') {
+        styles = {
+            ...styles,
+            border: "1px solid black",
+            backgroundColor: "transparent"
+        };
+    } else if(variant === 'contained') {  
+        styles = {
+            ...styles,
+            backgroundColor: "black",
+            color: "white"
+        };
+    }     
+  return (
+    <button style={styles}>{title}</button>  
+  )
+}
+
+
+export default Button;
+
+
+export interface ButtonProps {
+    title: string;
+    variant?: 'outlined' | 'contained';
+}
